7358c415433a7bb2ce77f3db7a4c52b799f9b4d5
Nicholas Boulanger <nicholasboulanger@sbcglobal.net>
2023-01-17 19:04:32 -0600

    getting started with uploading excel file, but need to make some changes before we begin

--- 2022 mouse data analysis.R
+++ 2022 mouse data analysis.R
@@ -1,1 +1,7 @@
 #2022 mouse data analysis
+
+#Load initial data
+install.packages("readxl")
+library(readxl)
+initial_data <- read_excel("C:\\Users\\nicho\\Documents\\001 Nicholas Documents\\Employment\\AAA BCI\\mouse paper\\Season 2 data.xlsx")
+View(initial_data)
